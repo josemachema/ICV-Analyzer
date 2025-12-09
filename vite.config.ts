8a619f20eb4c138d756f143b9f01a673c32a1326
Geronimo9205 <58924163+josemachema@users.noreply.github.com>
2025-12-09 06:54:24 -0700

    Modificando el aplicativo, bueno no me acuerdo si hice commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // o simplemente quita la línea base
   plugins: [react()],
-  base: "/ICV-Analyzer/",
 })
